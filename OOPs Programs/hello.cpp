8e2dc0011cec83dedee48ec9b5d9da242ec52cdc
Harsh-Jindal01 <143272897+Harsh-Jindal01@users.noreply.github.com>
2024-08-29 06:41:15 +0000

    Final testing of C++

---
+++ OOPs Programs/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+
+/*
+Terminal command to run this code in terminal is: 
+
+g++ file_name(with extention) -o fileName
+./fileName
+*/
